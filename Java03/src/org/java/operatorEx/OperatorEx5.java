3afcec6e86a37b08efc69246e1a3035285f90b3c
chominsung <chominsung@gmail.com>
2023-12-28 17:43:41 +0900

    논리연산자

--- Java03/src/org/java/operatorEx/OperatorEx5.java
+++ Java03/src/org/java/operatorEx/OperatorEx5.java
@@ -44,6 +44,31 @@ public class OperatorEx5 {
 		}
 		
 		
+		// 아이디 와 비밀번호를 입력 받아서 
+		// 입력받은 아이디가 dbId와 입력받은 비밀번호가 dbPw가 동시에 일치 하면
+		// 콘솔에 "로그인 성공" 실패 하면 "로그인 실패"가 출력 되도록 프로그래밍 하시오.
+		// scanner , equlas , && , if~else문 을 활용하여 작성하시오.
+		// 입력 받은 값을 변수명은 임의로 작성
+		String dbId="green";
+		String dbPw="1111";
+		
+		System.out.println(dbId.equals("green"));
+		System.out.println(dbPw.equals("1111"));
+		
+		System.out.println(dbId.equals("green") && dbPw.equals("1111"));
+		
+		if(dbId.equals("green")&& dbPw.equals("1111")) 
+		if(dbId.equals("Yellow")&& dbPw.equals("3333")) {	
+			System.out.println("로그인 성공");
+		}else {
+			System.out.println("로그인 실패");
+		}
+		
+		
+		
+		
+		
+		
 		
 	}
 }
